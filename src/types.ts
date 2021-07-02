c3a89f1f5597ea7feb78e800a0631a815c72de27
dj-viking <anders.swedishviking@gmail.com>
2021-07-01 22:02:36 -0400

    refactoring the listItemClass

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,16 @@
 /**
  * image will be a URL link to get load an image
  */
-export interface Memory {
+export interface Memory extends Object {
   id: number;
   name: string;
   image: string;
   description: string;
   route: string;
 }
-export class ListItemClass extends Object implements Memory { 
-  id!: number;
-  name!: string;
-  description!: string;
-  image!: string;
-  route!: string;
+export class ListItemClass implements Memory { 
+  id!: number; route!: string; name!: string; description!: string; image!: string;
   constructor(memory: Memory) {
-    super();
     Object.assign(this, {
       ...memory
     });
