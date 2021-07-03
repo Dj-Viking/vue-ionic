6b91869b8a7aafca3b835c86451381f2dbd4d407
dj-viking <anders.swedishviking@gmail.com>
2021-07-03 13:35:44 -0400

    added in postgres connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,33 @@
-console.log("hello worlasdfasdfafsad");
+require('dotenv').config();
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+// import express from "express";
+// import cors from "cors";
+// import Redis from "ioredis";
+// import session from "express-session";
+// import connectRedis from "connect-redis";
+// import { COOKIE_NAME } from "constants";
+const {
+  DB_NAME,
+  DB_USER,
+  DB_TYPE,
+  DB_PASSWORD
+} = process.env;
+
+(async function Main(){
+  console.log("hello world!!!!");
+  
+  const connection = await createConnection({
+    type: DB_TYPE as PostgresConnectionOptions["type"],
+    database: DB_NAME as string,
+    username: DB_USER as string,
+    password: DB_PASSWORD as string,
+    logging: true,
+    synchronize: true, //usually true during dev
+    entities: []
+  });
+  console.log("connection", connection);
+  
+
+})().catch(console.log);
