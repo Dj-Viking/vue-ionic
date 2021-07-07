d0dda34c48ab049acc2f9e2cde1f61bb0eaa2b21
dj-viking <anders.swedishviking@gmail.com>
2021-07-07 10:38:39 -0400

    abstracted out some constants to env vars

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,12 +1,20 @@
+require("dotenv").config();
 import { RegisterResponse } from "./types";
+
+const {
+  TEST_EMAIL,
+  TEST_HOST,
+  TEST_PASS,
+  TEST_USER
+} = process.env;
 
 export const COOKIE_NAME: string = "sid";
 export const IS_PROD: boolean | undefined = process.env.NODE_ENV === "production";
 export const FORGET_PASS_PREFIX: string = "forget-password:";
-export const HOST: string = "http://localhost:4000";
-export const REGISTER_EMAIL: string = "viking@viking.com"
-export const REGISTER_USERNAME: string = "viking"
-export const REGISTER_PASSWORD: string = "viking"
+export const HOST: string | undefined = TEST_HOST;
+export const REGISTER_EMAIL: string | undefined = TEST_EMAIL
+export const REGISTER_USERNAME: string | undefined = TEST_USER
+export const REGISTER_PASSWORD: string | undefined = TEST_PASS
 export const REGISTER_MUTATION: string = `
 mutation register {
   register(options: {
