d0b58c37297908b143e9ea03adc3e87711c0aa5f
dj-viking <anders.swedishviking@gmail.com>
2021-07-11 05:01:47 -0400

    formatting types

--- client/src/types.ts
+++ client/src/types.ts
@@ -2,77 +2,77 @@
  * image will be a URL link to get load an image
  */
 export interface Memory extends Object {
-  id: number;
-  name: string;
-  image: string;
-  description: string;
-  route: string;
+	id: number;
+	name: string;
+	image: string;
+	description: string;
+	route: string;
 }
-export class ListItemClass implements Memory { 
-  id!: number; route!: string; name!: string; description!: string; image!: string;
-  constructor(memory: Memory) {
-    Object.assign(this, {
-      ...memory
-    });
-  }
+export class ListItemClass implements Memory {
+	id!: number; route!: string; name!: string; description!: string; image!: string;
+	constructor(memory: Memory) {
+		Object.assign(this, {
+			...memory
+		});
+	}
 }
 /**
  * intended register mutation arguments
  */
 export interface RegisterArgs {
-  email: string;
-  username: string;
-  password: string;
+	email: string;
+	username: string;
+	password: string;
 }
 /**
  * the response expected after executing a register mutation
  */
 export interface RegisterResponse {
-  register: {
-      errors: null 
-      | [{
-        field: string;
-        message: string;
-      }];
-      user: null 
-      | {
-          email: string;
-      };
-  };
+	register: {
+		errors: null
+		| [{
+			field: string;
+			message: string;
+		}];
+		user: null
+		| {
+			email: string;
+		};
+	};
 }
 /**
  * intended login mutation arguments
  */
- export interface LoginArgs {
-  email: string;
-  password: string;
+export interface LoginArgs {
+	email: string;
+	password: string;
 }
 /**
  * the response expected after executing a login mutation
  */
 export interface LoginResponse {
-  login: {
-      errors: null 
-      | [{
-        field: string;
-        message: string;
-      }];
-      user: null 
-      | {
-          email: string;
-      };
-  };
+	login: {
+		errors: null
+		| [{
+			field: string;
+			message: string;
+		}];
+		user: null
+		| {
+			email: string;
+		};
+	};
 }
 /**
  * expected me query response
  */
 export interface MeQueryResponse {
-  errors: null 
-  | [{
-    field: string;
-    message: string;
-  }];
-  username: string;
-  id: number;
-  email: string;
+	errors: null
+	| [{
+		field: string;
+		message: string;
+	}];
+	username: string;
+	id: number;
+	email: string;
 }
