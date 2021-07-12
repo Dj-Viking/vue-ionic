c304bf3f220bdb61bb4a44d8cdaadbf8097e1914
dj-viking <anders.swedishviking@gmail.com>
2021-07-12 08:05:06 -0400

    adjusting

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,7 +37,8 @@ export const startServer = async (): Promise<void> => {
     logging: true, //dont log if we are in prod
     synchronize: !IS_PROD, //usually true during dev
     extra: {
-      ssl: IS_PROD ? { rejectUnauthorized: true } : false,
+      ssl: IS_PROD,
+      rejectUnauthorized: false
     },
     entities: [User]
   });
